bf9df1b692a175aaa0a08447224bbbc2b42db7ad
lucas solari <solarilucas92@gmail.com>
2024-08-02 16:11:02 +0200

    m

--- api/app.py
+++ api/app.py
@@ -1,14 +1,20 @@
 import os
 from flask import Flask, jsonify 
+from flask_migrate import Migrate
 from __init__ import create_app
 from models.models import db, Users
+from flask_cors import CORS
+
+
 app = create_app()
+MIGRATE = Migrate(app, db)
+CORS(app)
 
 
 @app.route('/', methods=['GET'])
 def hello():
     users = Users.query.all()
-    return jsonify({'success': True, 'data': [user.serialize() for user in users]}), 200
+    return jsonify({'users': [user.serialize() for user in users]}), 200
 
 
 if __name__ == '__main__':
